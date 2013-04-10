cc116ed16ed4a2abd368727aac4af98b3afb9a88
humx <humx1@spdb.com.cn>
2013-04-10 17:35:41 +0800

    20130410_complate_2C/2D_3A/3B

--- emu_hsm.py
+++ emu_hsm.py
@@ -56,6 +56,26 @@ class Hsm:
         )
         return result
 
+    def handle_1E(self,data):
+        """
+        （1E/1F）   在MK及KEK加密的密钥之间的转换
+        输入指令：2+1+1+(1A+3H)/（16/32/48）+1+16/32/48+4（可选）
+            2:'1E'
+            1:'1'->从MK加密到KEK加密(输入KEK索引) '2'->从KEK加密到MK加密（输入MK加密KEK密钥）
+            1:1->64bit,2->128bit,3->192bit
+            (1A+3H)/（16/32/48）：(1A+3H)->KEK索引：K+3N （16/32/48）->由MK加密的密钥
+            1：WK单倍长 1->64bit,2->128bit,3->192bit
+            16/32/48：MK或KEK加密下的密文
+            4（可选）：如果该字段存在，转换后的密钥保存至该索引号中  如‘K003’
+
+        输出结果：2+2+16/32/48+16
+            2:"1F"
+            2:错误代码，00表示正确
+            16/32/48:转换后KEK或MK加密下的WK
+            16:WK校验值
+        """
+        #待完成
+
     def handle_2A(self,data):
         """
         （2A/2B）   存储一个MK加密的密钥至指定的索引位置
@@ -82,12 +102,12 @@ class Hsm:
             return '2B22' #密钥长度与使用模式不符
         
         cipher=binascii.unhexlify(data[7:])
-        #print 'cipher:',binascii.hexlify(cipher)
+        print '2A:cipher:',binascii.hexlify(cipher)
         if len(cipher)!=int(keylen)*8:
             return '2B22' #密钥长度与使用模式不符
         k=pyDes.triple_des(self.HSM['lmk'])
         clear=k.decrypt(cipher)
-        #print 'clear:',binascii.hexlify(clear)
+        print '2A:clear:',binascii.hexlify(clear)
         self.setkey(keyindex,clear)
 
         if keylen=='1':
@@ -97,6 +117,111 @@ class Hsm:
         check=wk.encrypt('\x00'*8)
         result='2B00'+binascii.hexlify(check).upper()
         return result
+
+    def handle_2C(self,data):
+        """
+        （2C/2D）   读取一个指定的索引的密钥
+        输入指令：2+4
+            2:"2A"
+            4:"K"+3位16进制索引号(密钥在密码机中要存放的位置（如：K001）)
+           
+        输出结果：2+2+1+16/32/48+16
+            2:"2D"
+            2:错误代码，00表示正确
+            1:1->64bit,2->128bit,3->192bit
+            16/32/48:用主密钥加密的工作密钥密文
+            16:单、双、三倍长密钥加密64比特 0的结果。
+        """
+        code,hexindex=struct.unpack('3s3s',data[:6])
+        if code!='2CK':
+            return '2D60' #无此命令
+        try:
+            keyindex=int(hexindex,16)#取得密钥索引
+            if keyindex<1 or keyindex>4096:
+                raise ValueError
+        except ValueError:
+            return '2D33' #密钥索引错
+        
+        clear=self.getkey(keyindex)#取出密钥
+        print '2C:clear:',binascii.hexlify(clear)
+        k=pyDes.triple_des(self.HSM['lmk'])#使用MK加密密钥
+        cipher = k.encrypt(clear)
+        print '2C:cipher:',binascii.hexlify(cipher)
+        if len(cipher)%8!=0:#生成密钥长度
+            return '2D22'#密钥长度与使用模式不符
+        keylen='0'
+        if len(cipher)==8:
+            keylen='1'
+        if len(cipher)==16:
+            keylen='2'
+        if len(cipher)==24:
+            keylen='3'
+
+        if keylen=='0':
+            return '2D22'#密钥长度与使用模式不符
+        if keylen=='1':#生成校验码
+            wk=pyDes.des(clear)
+        else:
+            wk=pyDes.triple_des(clear)
+        check=wk.encrypt('\x00'*8)
+
+        result='2D00'+keylen+binascii.hexlify(cipher).upper()+binascii.hexlify(check).upper()
+        return result
+
+    def handle_3A(self,data):
+        """
+        （3A/3B）   生成密钥的校验值
+        输入指令：2+1+（1A+3H）/16/32/48
+            2:"3A"
+            1:1->64bit,2->128bit,3->192bit
+            4:"K"+3位16进制索引号(密钥在密码机中要存放的位置（如：K001）) 16/32/48:用主密钥加密的工作密钥密文
+           
+        输出结果：2+2+16
+            2:‘3B’
+            2:错误代码，00表示正确
+            16:单、双、三倍长密钥加密64比特 0的结果。
+        """   
+        code,keylen,codeindex=struct.unpack('2s1s1s',data[:4])
+        if code!='3A':
+            return '3B60' #无此命令
+        if codeindex=='K':#输入索引
+            temp1,temp2 = struct.unpack('2s1s',data[4:7])
+            hexindex=temp1+temp2
+            print '3A  hexindex:',hexindex
+            try:
+                keyindex=int(hexindex,16)
+                if keyindex<1 or keyindex>4096:
+                    raise ValueError
+            except ValueError:
+                return '3B33' #密钥索引错
+            clear=self.getkey(keyindex)#取出密钥
+            print '3A:clear:',binascii.hexlify(clear)
+            if len(clear)!=int(keylen)*8:
+                return '3B22' #密钥长度与使用模式不符
+
+            if keylen=='1':#生成校验码
+                wk=pyDes.des(clear)
+            else:
+                wk=pyDes.triple_des(clear)
+            check=wk.encrypt('\x00'*8)
+            result='3B00'+binascii.hexlify(check).upper()     
+        else:#输入MK加密的密钥
+            cipher=binascii.unhexlify(data[3:])
+            print '3A:cipher:',binascii.hexlify(cipher)
+            if len(cipher)!=int(keylen)*8:
+                return '3B22' #密钥长度与使用模式不符
+            k=pyDes.triple_des(self.HSM['lmk'])
+            clear=k.decrypt(cipher)#解密成明文
+            print '3A:clear:',binascii.hexlify(clear)
+
+            if keylen=='1':
+                wk=pyDes.des(clear)
+            else:
+                wk=pyDes.triple_des(clear)
+            check=wk.encrypt('\x00'*8)
+            result='2B00'+binascii.hexlify(check).upper()
+        return result            
+
 
 
     def load(self,fname):
@@ -128,5 +253,12 @@ if __name__=='__main__':
     hsm=Hsm('hsm.dat')
     print hsm.handle('HR')
     print hsm.handle('2AK00110123456789ABCDEF')
-    print hsm.handle('2AKFFF18A5AE1F81AB8F2DD')
+    print hsm.handle('2AK00220123456789ABCDEF0123456789ABCDEF')
+    print hsm.handle('2AKFFF38A5AE1F81AB8F2DD8A5AE1F81AB8F2DD8A5AE1F81AB8F2DD')
+    print hsm.handle('2CK001')
+    print hsm.handle('2CK002')
+    print hsm.handle('2CKFFF')
+    print hsm.handle('3A1K001')
+    print hsm.handle('3A2K002')
+    print hsm.handle('3A10123456789ABCDEF')
     hsm.save()
