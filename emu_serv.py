2ddd02acce777c209ede4a31ac6abac740b8ac98
yuzh <yuzh@spdb.com.cn>
2013-05-17 13:23:16 +0800

    emu_serv.py支持spdb开始的字符串作为控制指令，实现exit和addip的功能 增加sckcli.py作为客户端模拟程序，类似于lsckcli

--- emu_serv.py
+++ emu_serv.py
@@ -1,9 +1,20 @@
+#encoding=utf8
 #!/usr/bin/env python
-
+"""
+2013-05017:增加127.0.0.1为允许的客户端
+    支持spdb开始的内容作为控制指令，包括help、exit、addip
+"""
 import asyncore
 import socket
 import struct
 import emu_hsm
+
+def gen_pkg(buf):
+    return struct.pack('>h',len(buf))+buf
+
+def unpkg(buf):
+    pkglen=struct.unpack('>h',buf[:2])[0]
+    return (pkglen,buf[2:2+pkglen])
 
 class EmuHandler(asyncore.dispatcher_with_send):
     def __init__(self,sock,hsm,map=None):
@@ -14,6 +25,32 @@ class EmuHandler(asyncore.dispatcher_with_send):
     def sethsm(self,hsm):
         self.hsm=hsm
         print 'hsm set to ',`hsm`
+
+    def handle(self,Req):
+        if Req[:4]=='spdb':
+            buf=self.handle_cmd(Req[4:])
+        else:
+            buf=self.hsm.handle(Req)
+        return buf
+
+    def handle_cmd(self,cmd):
+        if cmd=='exit':
+            self.send(gen_pkg('bye'))
+            self.close()
+            print('HSM EMU Server shutdown!')
+            exit(0)
+        elif cmd[:4]=='help':
+            return 'HSM Emu server,support commands:\n'\
+                'help\n'\
+                'exit\n'\
+                'addip[ipaddr]\n'
+        elif cmd[:5]=='addip':
+            print 'origin',self.hsm.HSM['whitelist']
+            self.hsm.HSM['whitelist'].append(cmd[5:])
+            print 'new',self.hsm.HSM['whitelist']
+            return 'add ip ok'
+        else:
+            return 'unknown command'
 
     def handle_read(self):
         data = self.recv(8192)
@@ -27,7 +64,7 @@ class EmuHandler(asyncore.dispatcher_with_send):
                 if len(self.buf)>=cmdlen:
                     Req=self.buf[2:cmdlen]
                     self.buf=self.buf[cmdlen:]
-                    buf=self.hsm.handle(Req)
+                    buf=self.handle(Req)
                     Res=struct.pack('>h',len(buf))+buf
                     self.send(Res)
 
@@ -49,7 +86,12 @@ class EmuServer(asyncore.dispatcher):
         else:
             sock, addr = pair
             print 'Incoming connection from %s' % repr(addr)
-            handler = EmuHandler(sock,self.hsm)
+            print addr[0]
+            if addr[0] in self.hsm.HSM['whitelist'] or addr[0]=='127.0.0.1':
+                handler = EmuHandler(sock,self.hsm)
+            else:
+                sock.send(gen_pkg('You r not my client!'))
+                sock.close()
 
 server = EmuServer('0.0.0.0', 10008)
 asyncore.loop()
