57494165245abd498930385a1b316b8f00777cb0
YuZhihua <yuzh@spdb.com.cn>
2014-05-21 13:42:33 +0800

    修改数据库位置（和程序一致） 增加zabbix代理程序的安装等功能

--- passdb.py
+++ passdb.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # 2014.02.24 13:21:14 CST
 #Embedded file name: passdb.py
 #encoding=utf-8
@@ -7,8 +8,10 @@ import os
 import sqlite3
 import json
 import random
+import traceback
 __all__ = ['passdb']
 import ssh_util
+import logdb
 
 class passdb:
 
@@ -70,7 +73,7 @@ def gen_pass(PASSLEN):
 def check_args():
     if len(sys.argv)<2:
         return False
-    l={'add':5,'gen':5,'get':4,'del':4,'set':6}.get(sys.argv[1])
+    l={'add':5,'gen':5,'get':4,'del':4,'set':6,'load':3,'zbx':4}.get(sys.argv[1])
     if not l or len(sys.argv)!=l:
         return False
     return True
@@ -80,7 +83,7 @@ def conn_host(user,host,passwd):
         ssh=ssh_util.ssh_util(host,22,user,passwd)
         return ssh
     except Exception as e:
-        print('*** %s:Caught exception: %s: %s' % (host,e.__class__, e))
+        print('*** %s:conn_host: %s: %s' % (host,e.__class__, e))
         return None
 
 def set_passwd(user,host,passwd,newpass):
@@ -97,6 +100,7 @@ def add_host(db):
     msg={'add':time.ctime()}
     memo=json.dumps(msg)
     db.create(user,host,oldpass,memo)
+    return 'add ok'
 
 def add_verify_host(db):
     host,user,oldpass=sys.argv[2:]
@@ -104,14 +108,15 @@ def add_verify_host(db):
     if ssh:
         print('%s connect ok'%(host))
         #get more info by ssh.exec_command(),save to memos
-        add_host(db)
+        return add_host(db)
+    else:
+        return 'connect fail'
 
 def get_host(db):
     host,user=sys.argv[2:]
     rlt=db.retrieve(user,host)
     if rlt:
         password,memo=rlt
-        print('password is [%s]' % (password,))
         try:
             msg=json.loads(memo)
         except ValueError:
@@ -119,8 +124,15 @@ def get_host(db):
         print('--- memo ---')
         for k in msg.keys():
             print('%s => %s'%(k,msg[k]))
+        ret='get memo'
+        t=raw_input('input name:')
+        if t==os.getlogin():
+            print('password is [%s]' % (password,))
+            ret=ret+',password'
+        return ret+',ok'
     else:
         print('host not found!')
+        return 'host not found!'
 
 def set_host(db):
     host,user,key,value=sys.argv[2:]
@@ -135,14 +147,19 @@ def set_host(db):
         msg[key]=value.decode('utf-8')
         memo=json.dumps(msg)
         db.update(user,host,password,memo)
+        return 'set info ok'
     else:
         print('host not found!')
+        return 'host not found!'
 
 def gen_host_pass(db):
     host,user,oldpass=sys.argv[2:]
     rlt=db.retrieve(user,host)
     if rlt:
-        newpass=gen_pass(8)
+        if os.environ.get('MANUAL_PASSWORD'):
+            newpass=raw_input('input password:')
+        else:
+            newpass=gen_pass(8)
         oldpass_indb,memo=rlt
         try:
             msg=json.loads(memo)
@@ -155,11 +172,14 @@ def gen_host_pass(db):
         if ssh:
             print('update %s password from %s to %s' % (host,oldpass,newpass) )
             db.conn.commit()
+            return 'update password ok'
         else:
             print('update password fail!')
             db.conn.rollback()
+            return 'update password fail!'
     else:
         print('host not found!')
+        return 'host not found!'
 
 def del_host(db):
     host,user=sys.argv[2:]
@@ -169,32 +189,59 @@ def del_host(db):
         rlt=raw_input('password is [%s]\nmemo is [%s]\nyes/no?' % (password,memo)).lower()
         if rlt in ('y','yes'):
             db.delete(user,host)
+            return 'del host ok'
     else:
         print('host not found!')
+        return 'host not found!'
 
 def load_host(db):
     filename=sys.argv[2]
     ConvertTxtToDB(filename, db)
 
+def zbx_run(db):
+    host,cmd=sys.argv[2:]
+    rlt=db.retrieve('root',host)
+    if rlt:
+        password,memo=rlt
+        try:
+            zbx=ssh_util.zbx_util(host,22,'root',password)
+            ret=zbx.execute(cmd)
+            for k in ret.keys():
+                print('[%s]:%s'%(k,ret[k]))
+            return cmd+',ok'
+        except Exception as e:
+            print '*** zbx_run: %s: %s' % (e.__class__, e)
+            traceback.print_exc()
+            return '*** zbx_run: %s: %s' % (e.__class__, e)
+    else:
+        print('host not found!')
+        return 'host not found!'
+
+    return 'add ok'
 def main():
     usage="""Usage:
     %s add host user password
     %s gen host user oldpassword
-    %s set host memo_type memo_data
+    %s set host user memo_type memo_data
     %s get host user
     %s del host user
-    """ % ((sys.argv[0],)*5)
+    %s zbx host <status|start|stop|reg|unreg|install|remove>
+    """ % ((sys.argv[0],)*6)
     if not check_args():
         print(usage)
         quit()
     func={'add':add_verify_host,'gen':gen_host_pass,'get':get_host,'del':del_host,\
-        'load':load_host,'set':set_host}.get(sys.argv[1])
-    db = passdb('pass.db')
+        'load':load_host,'set':set_host,'zbx':zbx_run}.get(sys.argv[1])
+    db = passdb(os.path.dirname(sys.argv[0])+'/pass.db')
+    log = logdb.logdb(os.path.dirname(sys.argv[0])+'/logs.db',True)
     try:
-        func(db)
+        rlt=func(db)
         db.close()
-    except sqlite3.DatabaseError,e:
-        print `e`
+    except Exception as e:
+        print('*** func: %s: %s' % (e.__class__, e))
+        traceback.print_exc()
+        rlt='*** func: %s: %s' % (e.__class__, e)
+    log.create2(' '.join(sys.argv),rlt)
 
 if __name__ == '__main__':
     main()
